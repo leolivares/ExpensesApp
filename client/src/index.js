348a3fc98ca15d1f111e33c112885fa6887a2826
leolivares <leolivares@uc.cl>
2020-07-24 14:40:37 -0400

    Added redux toolkit :heavy_plus_sign:

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ client/src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import ReactFaker from 'react-faker';
+
+
+const store = configureStore({
+  reducer: {
+
+  },
+});
+
+export default store;
